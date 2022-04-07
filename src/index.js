08afb30354dbe4b4370826dc4d5fac8ec8ace401
seon <seonsy44@gmail.com>
2022-04-07 23:30:23 +0900

    #1.2 Actions

--- src/index.js
+++ src/index.js
@@ -4,9 +4,16 @@ const add = document.querySelector("#add");
 const minus = document.querySelector("#minus");
 const number = document.querySelector("span");
 
-const countModifier = (state = 0) => {
-  return state;
+const countModifier = (count = 0, action) => {
+  if (action.type === "ADD") {
+    return count + 1;
+  } else if (action.type === "MINUS") {
+    return count - 1;
+  } else {
+    return count;
+  }
 };
 
 const countStore = createStore(countModifier);
-console.log(countStore.getState());
+
+countStore.dispatch({ type: "ADD" });
