74405ca57ba5667ae0671dbc01054511a4634ab8
seon <seonsy44@gmail.com>
2022-04-07 23:42:19 +0900

    #1.4 Refactor

--- src/index.js
+++ src/index.js
@@ -5,14 +5,17 @@ const minus = document.querySelector("#minus");
 const number = document.querySelector("span");
 
 number.innerText = 0;
+const ADD = "ADD";
+const MINUS = "MINUS";
 
 const countModifier = (count = 0, action) => {
-  if (action.type === "ADD") {
-    return count + 1;
-  } else if (action.type === "MINUS") {
-    return count - 1;
-  } else {
-    return count;
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
   }
 };
 
@@ -25,10 +28,10 @@ const onChange = () => {
 countStore.subscribe(onChange);
 
 const handleAdd = () => {
-  countStore.dispatch({ type: "ADD" });
+  countStore.dispatch({ type: ADD });
 };
 const handleMinus = () => {
-  countStore.dispatch({ type: "MINUS" });
+  countStore.dispatch({ type: MINUS });
 };
 
 add.addEventListener("click", handleAdd);
